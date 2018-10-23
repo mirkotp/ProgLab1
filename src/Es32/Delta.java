e5b00df77ed2e95f992c18917d0351fb4328bbf4
mirkotp <mirko.tp@gmail.com>
2018-10-23 10:23:59 +0200

    sistemo Es3

---
+++ src/Es32/Delta.java
@@ -0,0 +1,46 @@
+package Es3;
+
+import prog.io.ConsoleInputManager;
+import prog.io.ConsoleOutputManager;
+
+/**
+ * Questo programma riceve in input dall'utente l'equazione di
+ * una parabola e ne calcola le eventuali soluzioni reali.
+ */
+public class Delta {
+	public static void main(String[] args) {
+
+		// Chiedo all'utente i parametri della parabola: a, b, c
+		ConsoleInputManager in = new ConsoleInputManager();
+		ConsoleOutputManager out = new ConsoleOutputManager();
+		double a, b, c;
+		
+		out.printf("Scrivi a: ");
+		a = in.readInt();
+
+		out.printf("Scrivi b: ");
+		b = in.readInt();
+
+		out.printf("Scrivi c: ");
+		c = in.readInt();
+
+		// Calcolo il delta della parabola e in base ad
+		// posso mostro il risultato corretto:
+		//
+		// dt < 0 non ha soluzioni.
+		// dt == 0 ha una soluzione.
+		// dt > 1 (else) ha due soluzioni.
+		double dt = (b * b) - (4 * a * c);
+
+		if (dt < 0) {
+			out.println("Non esistono soluzioni reali. (Prova con -a).");
+		} else if (dt == 0) {
+			out.println("L' equazione ha una sola soluzione.");
+			out.printf("X = %.2f \n", (-b) / 2 * a);
+		} else {
+			out.println("L' equazione ha due soluzioni.");
+			out.printf("X1 = %.2f \n", (-b - Math.sqrt(dt)) / 2 * a);
+			out.printf("X2 = %.2f \n", (-b + Math.sqrt(dt)) / 2 * a);
+		}
+	}
+}
